2c5abe1efdd28d86101ded3305a205dde9a955be
Sa4dUs <dmmarcelo27@gmail.com>
2023-05-04 14:12:43 +0200

    fix: :wrench: link path with enviromental variable

--- src/data/words.ts
+++ src/data/words.ts
@@ -1,6 +1,6 @@
 import fs from "fs"
 import path from "path"
 
-export const WORDS = fs.readFileSync(path.resolve(__dirname, '../../../../src/data/dict.txt')).toString().split('\n');
+export const WORDS = fs.readFileSync(path.resolve(__dirname, process.env.NEXT_PUBLIC_DICT_PATH || "")).toString().split('\n');
 
 export const SECRET_WORD = WORDS[Math.floor(Math.random()*WORDS.length)];
