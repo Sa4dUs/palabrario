13770f2230dd2391ace91997d6fae38cad63901c
Sa4dUs <dmmarcelo27@gmail.com>
2023-05-09 11:42:51 +0200

    fix: :ambulance: fix word update

--- src/pages/api/word/index.ts
+++ src/pages/api/word/index.ts
@@ -2,27 +2,26 @@ import randomWords from "random-spanish-words";
 
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(
+async function updateData() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_REDIS_URL}/set/wotd/${randomWords()}`,
+    {
+        headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_TOKEN}`,
+        },
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+   
+    return res.json();
+  }
+
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    if (req.method === 'GET' || req.method === 'POST') {
-    console.log(req.method);
-    try {
-        fetch(
-            `${process.env.NEXT_PUBLIC_REDIS_URL}/set/wotd/${randomWords()}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_TOKEN}`,
-                },
-            }
-        );
+    await updateData();
 
-        res.send("OK");
-    } catch (err) {
-        res.status(500).send(err);
-    } finally {
-        res.end();
-    }
-}
+    res.send('OK');
 }
